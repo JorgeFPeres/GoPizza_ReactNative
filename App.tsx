35f3bc07b3c68bbac96ded9ba86e40e9afb7de14
jorgefperes <jorge-peres@live.com>
2022-01-29 09:20:52 -0300

    chore: app loading

--- App.tsx
+++ App.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
     DMSerifDisplay_400Regular,
   })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
 
   return (
     <View>
