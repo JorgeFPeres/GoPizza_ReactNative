ccb311fa78645df975beecbb79959f9490e1c7a2
jorgefperes <jorge-peres@live.com>
2022-01-29 10:36:44 -0300

    feat: config status bar pattern

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
 import AppLoading from 'expo-app-loading'
+import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components/native'
 import theme from './src/theme'
 
@@ -20,6 +21,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style='light' backgroundColor='transparent' translucent />
       <SignIn></SignIn>
     </ThemeProvider>
   )
