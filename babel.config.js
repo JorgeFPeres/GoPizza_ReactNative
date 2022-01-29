e2a768758e5662a496aed73a0c6b7e721eee4dc4
jorgefperes <jorge-peres@live.com>
2022-01-29 09:13:19 -0300

    chore: add typescript and path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo']
-  };
-};
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+          },
+        },
+      ],
+    ],
+  }
+}
