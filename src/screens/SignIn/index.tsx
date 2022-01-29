01d31116de12720be9fc43307f6380de086b9f5e
jorgefperes <jorge-peres@live.com>
2022-01-29 11:48:32 -0300

    feat: SignIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,19 +1,41 @@
 import React from 'react'
-import { Container } from './styles'
+import { KeyboardAvoidingView, Platform } from 'react-native'
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+} from './styles'
+import brandImg from '@assets/brand.png'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder='E-mail'
-        type='secondary'
-        autoCorrect={false}
-        autoCapitalize='none'
-      />
-      <Input placeholder='Senha' type='secondary' secureTextEntry />
-      <Button title='Entrar' type='secondary' />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder='E-mail'
+            type='secondary'
+            autoCorrect={false}
+            autoCapitalize='none'
+          />
+          <Input placeholder='Senha' type='secondary' secureTextEntry />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button title='Entrar' type='secondary' />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
