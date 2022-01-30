71a07abda0188abf437af76bdcc08b34ecc44278
jorgefperes <jorge-peres@live.com>
2022-01-30 08:53:49 -0300

    feat: product screen

--- App.tsx
+++ App.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
-import AppLoading from 'expo-app-loading'
 import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components/native'
 import { AuthProvider } from '@hooks/auth'
 import theme from './src/theme'
+import { Product } from '@screens/Product'
 import { Text } from 'react-native'
-
-import { SignIn } from '@screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +23,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <StatusBar style='light' backgroundColor='transparent' translucent />
       <AuthProvider>
-        <SignIn />
+        <Product />
       </AuthProvider>
     </ThemeProvider>
   )
